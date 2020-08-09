82d97168836f34e714bedd32a3aec2fbdfaa7c54
Andre Kley <gigakley@gmail.com>
2020-08-09 20:25:10 -0300

    Seed de categorias

--- app/Http/Controllers/TarefaController.php
+++ app/Http/Controllers/TarefaController.php
@@ -2,8 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Model\Categoria;
 use App\Repositories\TarefaRepositoryInterface;
+
+
 use Illuminate\Http\Request;
+use Log,Validator;
 
 class TarefaController extends Controller
 {
@@ -11,44 +15,82 @@ class TarefaController extends Controller
     {
         $this->tarefaRepository=$tarefaRepository;
     }
+    public function index(){
+        $categorias=Categoria::pluck('categorias','id');
+        return view('index',compact('categorias'));
+    }
 
     public function listar(){
         try {
-            return $this->tarefaRepository->listar();
+            $lista=$this->tarefaRepository->listar();
         }catch (\Exception $e){
-            return Log::error('Log Error.', $e);
+            return $e->getMessage();
         }
+        return view('lista-tarefas',compact('lista'));
 	}
 
-    public function adicionar(Request $request,$id){
+    public function adicionar(Request $request){
         try {
-            return $this->tarefaRepository->adicionar($request, $id);
-        }catch (\Exception $e){
-            return Log::error('Log Error.', $e);
+            $this->tarefaRepository->adicionar($request);
+            }catch (\Exception $e){
+                return $e->getMessage();
         }
+         return redirect('tarefas/adicionar')->with('status','Cadastro');
+
     }
 
-    public function filtrar($id){
+    public function filtrar(Request $cat){
         try {
-            return $this->tarefaRepository->filtro($id);
+            $cat=$cat->query('cat');
+            $lista=$this->tarefaRepository->filtro($cat);
         }catch (\Exception $e){
-            return Log::error('Log Error.', $e);
+            return $e->getMessage();
         }
+        return view('lista-tarefas',compact('lista'));
+
     }
 
     public function atualizar(Request $request,$id){
         try {
-            return $this->tarefaRepository->atualizar($request, $id);
+            $this->tarefaRepository->atualizar($request, $id);
         }catch (\Exception $e){
-            return Log::error('Log Error.', $e);
+            return $e->getMessage();
+
         }
+        return redirect()->route('update',['id'=>$id])->with('status','Tarefa Alterada');
+
     }
 
     public function finalizar(Request $request,$id){
         try {
-            return $this->tarefaRepository->finalizarTarefa($request, $id);
+            $this->tarefaRepository->finalizarTarefa($request, $id);
         }catch (\Exception $e){
-            return Log::error('Log Error.', $e);
+            return $e->getMessage();
         }
+        return redirect()->back()->with('status','Tarefa Removida');
+
     }
+
+    public function visualizar($id){
+        try {
+            $info=$this->tarefaRepository->visualizar($id);
+        }catch (\Exception $e){
+            return $e->getMessage();
+        }
+        return view('info',compact('info'));
+
+    }
+
+    public function update($idTarefa){
+        try {
+            $categorias=Categoria::pluck('categorias','id');
+            $update=$this->tarefaRepository->realizarAtualizacao($idTarefa);
+        }catch (\Exception $e){
+            return $e->getMessage();
+        }
+        return view('update',compact('update','categorias'));
+    }
+
+
+
 }
