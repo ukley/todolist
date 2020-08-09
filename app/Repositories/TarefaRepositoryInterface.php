45a01d43e3aa7bd656cc434dd13e80827ba7b452
Andre Kley <gigakley@gmail.com>
2020-08-09 20:24:06 -0300

    Atividades de tarefa

--- app/Repositories/TarefaRepositoryInterface.php
+++ app/Repositories/TarefaRepositoryInterface.php
@@ -6,11 +6,16 @@ interface TarefaRepositoryInterface
 {
     public function listar();
 
-    public function adicionar($request, $id);
+    public function adicionar($request);
 
     public function atualizar($request, $id);
 
     public function filtro($id);
 
+    public function visualizar($id);
+
     public function finalizarTarefa($request, $id);
-}
+
+    public function realizarAtualizacao($idTarefa);
+
+    }
